da864494aea19a758820dc5ee9c289d661fa02af
danilo marichal <danilo.marichal@icloud.com>
2017-06-28 14:24:24 -0400

    adds changes

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const methodOverride = require('method-override');
 var pg = require('pg');
 const bcrypt = require('bcrypt');
 const salt = bcrypt.genSalt(10);
+var multer  = require('multer');
+var path = require('path');
+var upload = multer({ dest: 'public/uploads/' })
+
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
@@ -16,6 +20,9 @@ app.use("/", express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'))
 app.use(bodyParser.json());
+
+
+
 
 app.use(session({
   secret: 'TRAVELER',
@@ -61,6 +68,20 @@ app.get('/', function(req,res){
     }
  });
 
+app.get('/user', function(req,res){
+    if(req.session.user) {
+      let data = {
+        "logged_in": true,
+        "email": req.session.user.email
+      };
+      res.render('user/index', data);
+
+    } else {
+      res.render('user/index');
+    }
+ });
+
+
 //USES USER's ENTERED INFO AND COMPARES
 //EMAIL AND PASSWORD
 //TO ALLOW ACCESS OR DENY IT
@@ -69,7 +90,7 @@ app.post('/login', function(req, res){
  db
   .one("SELECT * FROM users WHERE email = $1", [data.email])
   .catch(function(){
-    res.send("Authorization Failed: Invalid email/password")
+    res.render("error/index");
   })
   .then(function(user){
     bcrypt.compare(data.password, user.password_digest, function(err, cmp){
@@ -77,17 +98,36 @@ app.post('/login', function(req, res){
         req.session.user = user;
         res.redirect("/");
       } else {
-        res.send("Authorization Failed: Invalid email/password");
+        res.render("error/index");
       }
     })
   })
 });
+
+//home
+app.get('/home', function(req, res){
+  res.render("login/index");
+})
 
 //RENDERS SIGN UP PAGE
 app.get('/signup', function(req, res){
   res.render('signup/index');
 });
 
+//Users profile
+app.get('/user', function(req, res){
+  res.render('user/index');
+});
+
+app.get('/user', function(req, res){
+  db.one("SELECT * FROM users WHERE id = " + id)
+  .then(function(data){
+    var one_user = {
+      email: data.email
+  };
+   res.render('user/index', one_user);
+  });
+});
 
 //ASKS FOR REQUIRED INFO IN ORDER TO SIGN UP
 //CREATES NEW USER IN DATABASE
@@ -118,7 +158,7 @@ app.get('/logout', function(req, res){
 
 //SELECTS LIST OF 10 DESTINATIONS FROM DATABASE "CITIES"
 app.get('/places', function(req, res) {
-  db.any("SELECT * FROM places")
+  scdb.any("SELECT * FROM places")
   .then(function(data){
     var all_plans = {
       places: data
@@ -131,7 +171,7 @@ app.get('/places', function(req, res) {
 //SELECTS ONE BY ONE IDs OF ELEMENTS TO BE SHOWN FROM DATABASE "CITIES"
 app.get('/places/:id', function(req, res){
   var id = req.params.id;
-  db.one("SELECT * FROM places WHERE id = " + id).then(function(data){
+  scdb.one("SELECT * FROM places WHERE id = " + id).then(function(data){
     var one_place = {
       name: data.name,
       img_url: data.img_url,
@@ -162,17 +202,47 @@ app.get('/posts', function(req, res) {
 
 //ACTUAL POST ROUTE THAT RETRIEVES POSTS INFO AND SAVES
 //THEM IN DATABASE.
-app.post("/posts/", function(req, res){
+/*
+app.post("/posts", function(req, res){
 country = req.body.country;
 url_image = req.body.url_image;
 comment = req.body.comment;
+if(country===""||url_image===""||comment===""){
+    res.render("error/show");
+}else{
 db.one("insert into posts(country, url_image, comment)VALUES($1,$2,$3) returning id",[country,url_image,comment])
 .then(data=>{
-console.log(data.id);
 res.redirect('/posts/'+data.id);
 });
-});
+}
+});*/
 //------------------------------------------------------
+//UPDATE EMAIL
+app.put('/user', function(req, res){
+    db.none("UPDATE users SET email = $1 WHERE email = $2",
+      [req.body.email, req.session.user.email]
+    ).catch(function(){
+      res.send('Failed to update user.');
+    }).then(function(){
+      res.send('User updated.');
+    });
+
+});
+/*
+//UPDATE PASSWORD
+app.post('/user', function(req, res){
+  let pass = req.body.password;
+  console.log(pass);
+  bcrypt
+    .hash(pass, 10, function(err, hash){
+      console.log(pass)
+      db.none("INSERT INTO users(email, password_digest) VALUES($1, $2)",[data.email, hash])
+      .then(function(){
+        res.send("updated");
+    });
+  });
+});
+*/
 //ROUTE THAT SENDS ALL POSTS TO COMMENTS INDEX FILE
 app.get('/comments', function(req, res){
     db.any("SELECT * FROM posts")
@@ -239,9 +309,21 @@ app.delete ('/user', function(req, res){
 
  });
 
+//Multer upload
 
+app.post('/posts', upload.single('upl'), function(req,res){
+ var country = req.body.country;
+ var file =req.file.path.substring(6);
+ var comment=req.body.comment;
+  db.one("INSERT INTO posts (country, url_image, comment) VALUES ($1, $2, $3) returning id",
+      [country, file, comment]
+      ).then(function(data){
+    res.redirect('posts/'+data.id);
+    });
 
-var port = process.env.PORT || 5000
+});
+
+var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
   console.log("Server Running {^-^}");
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,6 +2,79 @@ $(".go-back").on('click', function(){
   window.history.go(-1);
 });
 
+$(".go-go").on('click', function(){
+  window.history.go(-1);
+});
+
+function clickCounter(){
+  if(typeof(Storage)!=="undefined"){
+    if(localStorage.clickC){
+      localStorage.clickCount=Number(localStorage.clickC)+1;
+    }else{
+      localStorage.clickC=1;
+    }
+    $('#sum1').html(localStorage.clickC);
+
+  }else{
+   $('#sum1').html("broken");
+  }
+}
+
+function clickCounterO(){
+  if(typeof(Storage)!=="undefined"){
+    if(localStorage.clickCo){
+      localStorage.clickCo=Number(localStorage.clickCo)+1;
+    }else{
+      localStorage.clickCo=1;
+    }
+    $('#sum2').html(localStorage.clickCo);
+  }else{
+   $('#sum2').html("broken");
+  }
+}
+
+function clickCounterT(){
+  if(typeof(Storage)!=="undefined"){
+    if(localStorage.clickCou){
+      localStorage.clickCou=Number(localStorage.clickCou)+1;
+    }else{
+      localStorage.clickCou=1;
+    }
+    $('#sum3').html(localStorage.clickCou);
+  }else{
+   $('#sum3').html("broken");
+  }
+}
+
+function clickCounterTh(){
+  if(typeof(Storage)!=="undefined"){
+    if(localStorage.clickCoun){
+      localStorage.clickCoun=Number(localStorage.clickCoun)+1;
+    }else{
+      localStorage.clickCoun=1;
+    }
+    $('#sum4').html(localStorage.clickCoun);
+  }else{
+   $('#sum4').html("broken");
+  }
+}
+
+function clickCounterF(){
+  if(typeof(Storage)!=="undefined"){
+    if(localStorage.clickCount){
+      localStorage.clickCount=Number(localStorage.clickCount)+1;
+    }else{
+      localStorage.clickCount=1;
+    }
+    $('#sum5').text(localStorage.clickCount);
+}else{
+   $('#sum5').html("broken");
+  }
+}
+
+
+//----------------------------------------------------
+
 $('.bttn').click(function(e){
  event.preventDefault();
  $input = $('#country-info').val();
@@ -26,13 +99,13 @@ var callAjax = function(){
   var currency = data[0].currencies[0].name;
 
 
-  $('.country').text("Country: "+data[0].name);
-  $('.code').text("Two letters code: "+code);
-  $('.region').text("Region :"+region);
-  $('.capital').text("The capital city is: "+capital);
-  $('.languages').text("Language/s: "+languages1);
-  $('.population').text("This country has a population of: "+population+" habitants.");
-  $('.currency').text("This country's currency is: "+currency);
+  $('#country').text("Country: "+data[0].name);
+  $('#code').text("Two letters code: "+code);
+  $('#region').text("Region :"+region);
+  $('#capital').text("The capital city is: "+capital);
+  $('#languages').text("Language/s: "+languages1);
+  $('#population').text("This country has a population of: "+population+" habitants.");
+  $('#currency').text("This country's currency is: "+currency);
   $('#flag').attr('src',data[0].flag);
 
     }
